b75ae097b35f48900ae8764e18369232a2ffe843
blessingchanel2 <blessingchanel2@gmail.com>
2026-06-12 15:46:20 +0100

    Update job title from 'WordPress Designer' to 'Graphic Designer'

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings: ["Frontend Developer", "Backend Developer ", "UI/UX Designer", "Graphic Designer"],
+    strings: ["Frontend Developer", "Backend Developer ", "UI/UX Designer", "WordPress Designer", "Graphic Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
